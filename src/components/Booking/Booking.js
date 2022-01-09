e625b14092b3fd2cde67485b116c4e48a4181f3d
Md Sakibul Islam <86571674+Md-Sakibul-Islam@users.noreply.github.com>
2022-01-09 12:21:35 +0600

    footer issue solved

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -8,91 +8,123 @@ import "./Booking.css";
 
 const Booking = () => {
   const history = useHistory();
-  // 
-    const[date,setDate]= useState('')
-    const[address,setAddress]= useState('');
+  //
+  const [date, setDate] = useState("");
+  const [address, setAddress] = useState("");
 
   //
-const{user}= useAuth();
-const[booked,setBooked]= useState({});
-const{id}= useParams();
-const url = `http://localhost:5000/travelbooking/${id}`;
-useEffect(()=>{
-  fetch(url)
-  .then(res => res.json())
-  .then(data => setBooked(data))
-},[])
+  const { user } = useAuth();
+  const [booked, setBooked] = useState({});
+  const { id } = useParams();
+  const url = `http://localhost:5000/travelbooking/${id}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBooked(data));
+  }, []);
 
-// onSubmit handler 
-const handleSubmitForm = e => {
-  e.preventDefault()
- const booking = {name:user.displayName,email:user.email,title:booked.title,date:date,address:address}
+  // onSubmit handler
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const booking = {
+      name: user.displayName,
+      email: user.email,
+      title: booked.title,
+      date: date,
+      address: address,
+    };
 
- 
-  // hitting post api
- fetch('http://localhost:5000/booking',{
-   method:'post',
-   headers:{
-     'content-type':'application/json'
-   },body:JSON.stringify(booking)
- })
- .then(res => res.json())
- .then(data => {
-   if(data.insertedId){
-     alert('Booking Successfully')
-     history.push('/mybooking')
-  
-   }
- })
+    // hitting post api
+    fetch("http://localhost:5000/booking", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("Booking Successfully");
+          history.push("/mybooking");
+        }
+      });
+  };
 
-
-}
-
-const handleDateChange = e => {
-setDate(e.target.value)
-}
-// function for address input field 
-const handleAddressChange = e => {
-  setAddress(e.target.value);
-}
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+  // function for address input field
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
 
   return (
     <div className="container-fluid booking">
-     <div>
-       {/* Booking form Area start */}
-     <Row>
-        <Col className="mx-auto" sm={12} md={5} lg={5}>
-         <div className="container main-form-div">
-           <div className="text-center mb-2"><h4>Infinity <span className="text-danger">Tour</span></h4></div>
-         <Form onSubmit={handleSubmitForm} className="form-container">
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Control type="text" readOnly  value={user.displayName || ''} placeholder="Name"/>
-            </Form.Group>
+      <div>
+        {/* Booking form Area start */}
+        <Row>
+          <Col className="mx-auto" sm={12} md={5} lg={5}>
+            <div className="container main-form-div">
+              <div className="text-center mb-2">
+                <h4>
+                  Infinity <span className="text-danger">Tour</span>
+                </h4>
+              </div>
+              <Form onSubmit={handleSubmitForm} className="form-container">
+                <Form.Group className="mb-3" controlId="formBasicText">
+                  <Form.Control
+                    type="text"
+                    readOnly
+                    value={user.displayName || ""}
+                    placeholder="Name"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control type="email" readOnly  value={user.email || ''} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Control type="text" readOnly  value={booked.title || ''} />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    readOnly
+                    value={user.email || ""}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicText">
+                  <Form.Control
+                    type="text"
+                    readOnly
+                    value={booked.title || ""}
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-              <Form.Control type="text" onChange={handleDateChange} placeholder="dd/mm/yyyy" />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicText">
+                  <Form.Control
+                    type="text"
+                    onChange={handleDateChange}
+                    placeholder="dd/mm/yyyy"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Control as="textarea" rows={3} onChange={handleAddressChange}  placeholder="Address"/>
-            </Form.Group>
-            <Button variant="success" type="submit">
-              Submit
-            </Button>
-          </Form>
-         </div>
-          {/* Booking form Area End */}
-        </Col>
-      </Row>
-     </div>
-    
+                <Form.Group className="mb-3" controlId="formBasicText">
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    onChange={handleAddressChange}
+                    placeholder="Address"
+                  />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                  Submit
+                </Button>
+              </Form>
+             
+            </div>
+           
+            {/* Booking form Area End */}
+          </Col>
+         
+        </Row>
+        
+      </div>
     </div>
   );
 };

f752c6c259b3a172f430381f9e521b0239e9db3b
Md Sakibul Islam <86571674+Md-Sakibul-Islam@users.noreply.github.com>
2022-01-13 11:42:07 +0600

    spinner added

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Row } from "react-bootstrap";
+import { Row, Spinner } from "react-bootstrap";
 import ShowDestination from "../ShowDestination/ShowDestination";
-import './Destination.css'
+import "./Destination.css";
 const Destination = () => {
   // tours state
   const [tours, setTours] = useState([]);
-// fetch tours all data
+  const [isLoading, setIsLoading] = useState(false);
+  // fetch tours all data
 
   useEffect(() => {
     setTimeout(() => {
+      setIsLoading(true);
       fetch("https://ancient-scrubland-42936.herokuapp.com/tours")
-      .then((res) => res.json())
-      .then((data) => setTours(data));
-    }, 2000);
-  },[]);
+        .then((res) => res.json())
+        .then((data) => {
+          setTours(data);
+          setIsLoading(false);
+        });
+    }, 100);
+  }, []);
 
   return (
     <div className="container destination-container">
@@ -23,13 +28,15 @@ const Destination = () => {
         <h5 className="text-danger">Find Your Tour</h5>
         <h4>Here is Destination</h4>
         {/* showing all destination  */}
-            <Row xs={1} md={3} className="mt-3 g-4">
-                {
-                    tours.map(tour => <ShowDestination key={tour._id} tour={tour}></ShowDestination>)
-                }
-          </Row>
-        
-        
+        <Row xs={1} md={3} className="mt-3 g-4">
+          {isLoading ? (
+            <div className="mx-auto"><Spinner  animation="grow" variant="dark" /></div>
+          ) : (
+            tours.map((tour) => (
+              <ShowDestination key={tour._id} tour={tour}></ShowDestination>
+            ))
+          )}
+        </Row>
       </div>
     </div>
   );
